b13c7428c469f5fbb993792c246192c379183397
cristianh6 <cristian.casalaspro@gmail.com>
2022-03-18 07:18:45 -0300

    html1

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express();
+const path = require("path")
+const port = 3030;
+
+app.use(express.static("public"))
+
+
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "views", "home.html")))
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`)
+})
